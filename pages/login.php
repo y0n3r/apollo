5cd238c455e1326a7cd8ab1070aa9136b6970f9c
Yonas Hassen <yonas1@gmail.com>
2012-11-04 15:05:07 -0500

    New login page.

---
+++ pages/login.php
@@ -0,0 +1,46 @@
+<h1>Update your profile</h1>
+
+<section id="login">
+	<h4>Create, view, and update your profile to stay current on University of Phoenix events and services.</h4>
+
+	<p>
+		To register, please provide your eCampus email and password, or your IRN number. If it has been 
+		more than three years since you graduated and you've forgotten your IRN, please <a href="#">contact</a> 
+		the alumni office to obtain it.
+	</p>
+	<section class="small-form form-section">
+		<form name="login-ecampus" id="login-ecampus" action="#">
+			<ul class="unstyled">
+				<li>
+					<label for="login-email">eCampus email address</label>
+					<input type="text" name="login-email" id="login-email" required autofocus>
+				</li>
+				<li>
+					<label for="login-password">Password</label>
+					<input type="password" name="login-password" id="login-password" required><br>
+					<a href="#">Forgot password?</a>
+				</li>
+			</ul>
+			<span class="signup">
+				<a href="#">Continue</a>
+			</span><!-- .signup -->
+		</form><!-- #login-ecampus -->
+	</section><!-- .small-form -->
+	<section class="small-form-divider">
+		<img src="../statics/alumni/global/img/or-divider.png">
+	</section><!-- .small-form-divider -->
+	<section class="small-form form-section">
+		<form name="login-irn" id="login-irn" action="#">
+			<ul class="unstyled">
+				<li>
+					<label for="login-irn">IRN number</label>
+					<input type="text" name="login-irn" id="login-irn" required><br>
+					<a href="#">Forgot IRN number?</a>
+				</li>
+			</ul>
+			<span class="signup">
+				<a href="#">Continue</a>
+			</span><!-- .signup -->
+		</form><!-- #login-irn -->
+	</section><!-- .small-form -->
+</section><!-- #login -->
