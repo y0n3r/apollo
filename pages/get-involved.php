137c03b0022de4447791659aee6473550196f2f9
Yonas Hassen <yonas1@gmail.com>
2012-11-05 00:17:51 -0500

    [IN PROGRESS] Alumni Chapter Events page.

---
+++ pages/get-involved.php
@@ -0,0 +1,86 @@
+<section id="alumni-chapter-events">
+	<img class="banner" src="../statics/alumni/global/img/sandbox/banner/alumni-chapter-events.png">
+	<table>
+		<thead>
+			<tr>
+				<th>Chapter City</th>
+				<th>State</th>
+				<th>Events</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>
+					<img src="../statics/alumni/global/img/sandbox/thumb/50x50_1.jpg">
+					<a href="#">Augusta Alumni Chapter</a>
+					<span class="chapter-info">Founded May 21, 2012 | 89 Members</span>
+				</td>
+				<td><b>GA</b></td>
+				<td>
+					<a href="#">Pig Roast</a>
+					<span class="chapter-info">May 21, 2012</span>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<img src="../statics/alumni/global/img/sandbox/thumb/50x50_2.jpg">
+					<a href="#">Baton Rouge Alumni Chapter</a>
+					<span class="chapter-info">Founded May 21, 2012 | 89 Members</span>
+				</td>
+				<td><b>LA</b></td>
+				<td>
+					<a href="#">Homecoming Dance</a>
+					<span class="chapter-info">May 21, 2012</span>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<img src="../statics/alumni/global/img/sandbox/thumb/50x50_3.jpg">
+					<a href="#">Oahu Alumni Chapter</a>
+					<span class="chapter-info">Founded May 21, 2012 | 89 Members</span>
+				</td>
+				<td><b>HI</b></td>
+				<td>
+					<a href="#">Surf Contest</a>
+					<span class="chapter-info">May 21, 2012</span>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<img src="../statics/alumni/global/img/sandbox/thumb/50x50_4.jpg">
+					<a href="#">Cumberland Alumni Chapter</a>
+					<span class="chapter-info">Founded May 21, 2012 | 89 Members</span>
+				</td>
+				<td><b>MD</b></td>
+				<td>
+					<a href="#">Square Dance</a>
+					<span class="chapter-info">May 21, 2012</span>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<img src="../statics/alumni/global/img/sandbox/thumb/50x50_5.jpg">
+					<a href="#">Arlington Alumni Chapter</a>
+					<span class="chapter-info">Founded May 21, 2012 | 89 Members</span>
+				</td>
+				<td><b>VA</b></td>
+				<td>
+					<a href="#">Bar Crawl</a>
+					<span class="chapter-info">May 21, 2012</span>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<img src="../statics/alumni/global/img/sandbox/thumb/50x50_6.jpg">
+					<a href="#">Pittsburgh Alumni Chapter</a>
+					<span class="chapter-info">Founded May 21, 2012 | 89 Members</span>
+				</td>
+				<td><b>PA</b></td>
+				<td>
+					<a href="#">Steelers Game</a>
+					<span class="chapter-info">May 21, 2012</span>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</section><!-- #alumni-chapter-events -->
