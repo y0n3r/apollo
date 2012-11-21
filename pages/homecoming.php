40995769bd15627bd3e775eaf064aaa07ecce365
Ambrose Ferber <ambrose.ferber@syntax8080.com>
2012-11-20 19:43:59 -0700

    Photo Gallery
    Changes for Todd:
    (All of this only needs to load with photo gallery)
    _ Some of the instructions below may refer to things committed in the
    LAST commit, but not updated again on this one.
    _ Add all contents of "tn3" folder in css/plugins.
    _ Add TN3 jquery plugin file to JS load.
    _ index.php: reference tn3-apollo CSS file. Ref TN3 plugin file.
    _ /*____// Photo Grid and Gallery //____*/ section of custom.js handles
    some hover effects for the grid view and the TN3 init.
    _ Photos.css and Homecoming.php are pretty well gutted.
    _ Added a 'tn3' dir to the dam to have some images in the gallery. Can
    be ignored if you have other images that get populated.
    _ This commit also deletes a lot of cruft.

--- pages/homecoming.php
+++ pages/homecoming.php
@@ -112,77 +112,230 @@ Here is the markup for secondary pages, without content for ease of integration.
 										<li>
 											<h4>Image 1</h4>
 											<div class="tn3 description">My Image 1 description</div>
-											<a href="/dam/tn3/1.jpg"><img src="http://placehold.it/105x58" /></a>
+											<a href="/dam/tn3/1.jpg"><img src="/dam/tn3/thumbs/1.jpg" /></a>
 										</li>
 										<li>
 											<h4>Image 2</h4>
 											<div class="tn3 description">My Image 2 description</div>
-											<a href="/dam/tn3/2.jpg"><img src="http://placehold.it/105x58" /></a>
+											<a href="/dam/tn3/2.jpg"><img src="/dam/tn3/thumbs/2.jpg" /></a>
 										</li>
 										<li>
 											<h4>Image 3</h4>
 											<div class="tn3 description">My Image 3 description</div>
-											<a href="/dam/tn3/3.jpg"><img src="http://placehold.it/105x58" /></a>
+											<a href="/dam/tn3/3.jpg"><img src="/dam/tn3/thumbs/3.jpg" /></a>
 										</li>
+										<li>
+											<h4>Image 4</h4>
+											<div class="tn3 description">My Image 4 description</div>
+											<a href="/dam/tn3/4.jpg"><img src="/dam/tn3/thumbs/4.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 5</h4>
+											<div class="tn3 description">My Image 5 description</div>
+											<a href="/dam/tn3/5.jpg"><img src="/dam/tn3/thumbs/5.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 6</h4>
+											<div class="tn3 description">My Image 6 description</div>
+											<a href="/dam/tn3/6.jpg"><img src="/dam/tn3/thumbs/6.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 7</h4>
+											<div class="tn3 description">My Image 7 description</div>
+											<a href="/dam/tn3/7.jpg"><img src="/dam/tn3/thumbs/7.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 8</h4>
+											<div class="tn3 description">My Image 8 description</div>
+											<a href="/dam/tn3/8.jpg"><img src="/dam/tn3/thumbs/8.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 9</h4>
+											<div class="tn3 description">My Image 9 description</div>
+											<a href="/dam/tn3/9.jpg"><img src="/dam/tn3/thumbs/9.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 10</h4>
+											<div class="tn3 description">My Image 10 description</div>
+											<a href="/dam/tn3/11.jpg"><img src="/dam/tn3/thumbs/10.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 11</h4>
+											<div class="tn3 description">My Image 11 description</div>
+											<a href="/dam/tn3/11.jpg"><img src="/dam/tn3/thumbs/11.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 12</h4>
+											<div class="tn3 description">My Image 12 description</div>
+											<a href="/dam/tn3/12.jpg"><img src="/dam/tn3/thumbs/12.jpg" /></a>
+										</li>
+									</ul>
+								</div>
+							</div><!-- / Modal Body-->
+						</div>
+					</div>
+				</li>
+				
+				<li>
+					<div class="thumbnail">
+						<img src="http://placehold.it/208x121" alt="" />
+						<p><strong>Alabama</strong> August 4, 2012</p>
+						<div class="thumb-hover" data-toggle="modal" data-target="#modal-photo-02">
+							<p>Atlanta alumni cheered on the Greenjackets and reconnected with old friends.<p>
+							<p class="count">23 images</p>
+						</div>
+						
+						<div id="modal-photo-02" class="modal fade lb-gallery" tabindex="-1" role="dialog" aria-labelledby="Alabama, August 4, 2012" aria-hidden="true">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+								<h3><strong>Alabama</strong> August 4, 2012</h3>
+							</div>
+							<div class="modal-body">
+								<div class="gallery">
+									<ul>
 										<li>
 											<h4>Image 1</h4>
 											<div class="tn3 description">My Image 1 description</div>
-											<a href="/dam/tn3/1.jpg"><img src="http://placehold.it/105x58" /></a>
+											<a href="/dam/tn3/1.jpg"><img src="/dam/tn3/thumbs/1.jpg" /></a>
 										</li>
 										<li>
 											<h4>Image 2</h4>
 											<div class="tn3 description">My Image 2 description</div>
-											<a href="/dam/tn3/2.jpg"><img src="http://placehold.it/105x58" /></a>
+											<a href="/dam/tn3/2.jpg"><img src="/dam/tn3/thumbs/2.jpg" /></a>
 										</li>
 										<li>
 											<h4>Image 3</h4>
 											<div class="tn3 description">My Image 3 description</div>
-											<a href="/dam/tn3/3.jpg"><img src="http://placehold.it/105x58" /></a>
+											<a href="/dam/tn3/3.jpg"><img src="/dam/tn3/thumbs/3.jpg" /></a>
 										</li>
+										<li>
+											<h4>Image 4</h4>
+											<div class="tn3 description">My Image 4 description</div>
+											<a href="/dam/tn3/4.jpg"><img src="/dam/tn3/thumbs/4.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 5</h4>
+											<div class="tn3 description">My Image 5 description</div>
+											<a href="/dam/tn3/5.jpg"><img src="/dam/tn3/thumbs/5.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 6</h4>
+											<div class="tn3 description">My Image 6 description</div>
+											<a href="/dam/tn3/6.jpg"><img src="/dam/tn3/thumbs/6.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 7</h4>
+											<div class="tn3 description">My Image 7 description</div>
+											<a href="/dam/tn3/7.jpg"><img src="/dam/tn3/thumbs/7.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 8</h4>
+											<div class="tn3 description">My Image 8 description</div>
+											<a href="/dam/tn3/8.jpg"><img src="/dam/tn3/thumbs/8.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 9</h4>
+											<div class="tn3 description">My Image 9 description</div>
+											<a href="/dam/tn3/9.jpg"><img src="/dam/tn3/thumbs/9.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 10</h4>
+											<div class="tn3 description">My Image 10 description</div>
+											<a href="/dam/tn3/11.jpg"><img src="/dam/tn3/thumbs/10.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 11</h4>
+											<div class="tn3 description">My Image 11 description</div>
+											<a href="/dam/tn3/11.jpg"><img src="/dam/tn3/thumbs/11.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 12</h4>
+											<div class="tn3 description">My Image 12 description</div>
+											<a href="/dam/tn3/12.jpg"><img src="/dam/tn3/thumbs/12.jpg" /></a>
+										</li>
+									</ul>
+								</div>
+							</div><!-- / Modal Body-->
+						</div>
+					</div>
+				</li>
+				
+				<li>
+					<div class="thumbnail">
+						<img src="http://placehold.it/208x121" alt="" />
+						<p><strong>Alabama</strong> August 4, 2012</p>
+						<div class="thumb-hover" data-toggle="modal" data-target="#modal-photo-03">
+							<p>Atlanta alumni cheered on the Greenjackets and reconnected with old friends.<p>
+							<p class="count">23 images</p>
+						</div>
+						
+						<div id="modal-photo-03" class="modal fade lb-gallery" tabindex="-1" role="dialog" aria-labelledby="Alabama, August 4, 2012" aria-hidden="true">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+								<h3><strong>Alabama</strong> August 4, 2012</h3>
+							</div>
+							<div class="modal-body">
+								<div class="gallery">
+									<ul>
 										<li>
 											<h4>Image 1</h4>
 											<div class="tn3 description">My Image 1 description</div>
-											<a href="/dam/tn3/1.jpg"><img src="http://placehold.it/105x58" /></a>
+											<a href="/dam/tn3/1.jpg"><img src="/dam/tn3/thumbs/1.jpg" /></a>
 										</li>
 										<li>
 											<h4>Image 2</h4>
 											<div class="tn3 description">My Image 2 description</div>
-											<a href="/dam/tn3/2.jpg"><img src="http://placehold.it/105x58" /></a>
+											<a href="/dam/tn3/2.jpg"><img src="/dam/tn3/thumbs/2.jpg" /></a>
 										</li>
 										<li>
 											<h4>Image 3</h4>
 											<div class="tn3 description">My Image 3 description</div>
-											<a href="/dam/tn3/3.jpg"><img src="http://placehold.it/105x58" /></a>
+											<a href="/dam/tn3/3.jpg"><img src="/dam/tn3/thumbs/3.jpg" /></a>
 										</li>
 										<li>
-											<h4>Image 1</h4>
-											<div class="tn3 description">My Image 1 description</div>
-											<a href="/dam/tn3/1.jpg"><img src="http://placehold.it/105x58" /></a>
+											<h4>Image 4</h4>
+											<div class="tn3 description">My Image 4 description</div>
+											<a href="/dam/tn3/4.jpg"><img src="/dam/tn3/thumbs/4.jpg" /></a>
 										</li>
 										<li>
-											<h4>Image 2</h4>
-											<div class="tn3 description">My Image 2 description</div>
-											<a href="/dam/tn3/2.jpg"><img src="http://placehold.it/105x58" /></a>
+											<h4>Image 5</h4>
+											<div class="tn3 description">My Image 5 description</div>
+											<a href="/dam/tn3/5.jpg"><img src="/dam/tn3/thumbs/5.jpg" /></a>
 										</li>
 										<li>
-											<h4>Image 3</h4>
-											<div class="tn3 description">My Image 3 description</div>
-											<a href="/dam/tn3/3.jpg"><img src="http://placehold.it/105x58" /></a>
+											<h4>Image 6</h4>
+											<div class="tn3 description">My Image 6 description</div>
+											<a href="/dam/tn3/6.jpg"><img src="/dam/tn3/thumbs/6.jpg" /></a>
 										</li>
 										<li>
-											<h4>Image 1</h4>
-											<div class="tn3 description">My Image 1 description</div>
-											<a href="/dam/tn3/1.jpg"><img src="http://placehold.it/105x58" /></a>
+											<h4>Image 7</h4>
+											<div class="tn3 description">My Image 7 description</div>
+											<a href="/dam/tn3/7.jpg"><img src="/dam/tn3/thumbs/7.jpg" /></a>
 										</li>
 										<li>
-											<h4>Image 2</h4>
-											<div class="tn3 description">My Image 2 description</div>
-											<a href="/dam/tn3/2.jpg"><img src="http://placehold.it/105x58" /></a>
+											<h4>Image 8</h4>
+											<div class="tn3 description">My Image 8 description</div>
+											<a href="/dam/tn3/8.jpg"><img src="/dam/tn3/thumbs/8.jpg" /></a>
 										</li>
 										<li>
-											<h4>Image 3</h4>
-											<div class="tn3 description">My Image 3 description</div>
-											<a href="/dam/tn3/3.jpg"><img src="http://placehold.it/105x58" /></a>
+											<h4>Image 9</h4>
+											<div class="tn3 description">My Image 9 description</div>
+											<a href="/dam/tn3/9.jpg"><img src="/dam/tn3/thumbs/9.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 10</h4>
+											<div class="tn3 description">My Image 10 description</div>
+											<a href="/dam/tn3/11.jpg"><img src="/dam/tn3/thumbs/10.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 11</h4>
+											<div class="tn3 description">My Image 11 description</div>
+											<a href="/dam/tn3/11.jpg"><img src="/dam/tn3/thumbs/11.jpg" /></a>
+										</li>
+										<li>
+											<h4>Image 12</h4>
+											<div class="tn3 description">My Image 12 description</div>
+											<a href="/dam/tn3/12.jpg"><img src="/dam/tn3/thumbs/12.jpg" /></a>
 										</li>
 									</ul>
 								</div>
